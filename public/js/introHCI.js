f8205ab3240cf005d45e0b35b8917f0c58ec9485
Jeffrey Ha <jha@ucsd.edu>
2021-02-25 03:14:00 -0800

    lab completion

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,19 +9,41 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	console.log("Page ready");
- 	// initCamera();
- 	// initMap();
+	console.log('Page ready');
+ 	initCamera();
+ 	initMap();
  	initGestures();
  	initRSVPForm();
 }
 
 // init jQuery gestures  
 function initGestures() {
-	// add gestures listener here
+	$(function() {
+		$('.judge-img').bind('taphold', tapholdHandler);
+
+		function tapholdHandler(event) {
+			// get the id of the event source
+			var targetIdPrefix = event.target.id;
+			
+			// show bio
+			$(`#${targetIdPrefix}-bio`).show();
+		}
+	});
 }
 
 // init RSVP form submit listener
 function initRSVPForm() {
-  // add your code here
+	$('#rsvpForm').submit(function(e) {
+		e.preventDefault();
+		var rsvpEmail = $('#rsvpEmail').val();
+
+		$.post('/addRSVP', {
+			rsvpEmail: rsvpEmail
+		}, postCallback);
+	});
+
+	function postCallback(res) {
+		alert('RSVP form has been successfully submitted!');
+		$('#rsvpEmail').val(''); // clear out the form
+	}
 }
--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
-	// add your code here
+	// register our API key
+	L.mapquest.key = 'uZ8X42IQd7oIiMcsVGvLpAYMtZCTKUPr';
+
+	// set up our map
+	var map = L.mapquest.map('map', {
+		center: [32.87874553885033, -117.23592891753651],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	// add a market (UCSD) to the map
+	L.marker([32.88, -117.236]).addTo(map);
 }
